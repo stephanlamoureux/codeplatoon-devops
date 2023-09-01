cf2ae35e480eda2c63844332d587ab200b1e8734
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-09-01 19:26:53 -0400

    update comments

--- assignments/bash/debug.sh
+++ assignments/bash/debug.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-# Say you have a command that fails rarely. In order to debug it you need to capture its output but it can be time consuming to get a failure run. Write a bash script that runs the following script until it fails and captures its standard output and error streams to files and prints everything at the end. Bonus points if you can also report how many runs it took for the script to fail.
+# Say you have a command that fails rarely. In order to debug it you need to capture its output but it can be time consuming to get a failure run.
+# Write a bash script that runs the following script until it fails and captures its standard output and error streams to files and prints everything at the end.
+# Bonus points if you can also report how many runs it took for the script to fail.
--- assignments/bash/marco_polo.sh
+++ assignments/bash/marco_polo.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# Write bash functions marco and polo that do the following. Whenever you execute marco the current working directory should be saved in some manner, then when you execute polo, no matter what directory you are in, polo should cd you back to the directory where you executed marco. For ease of debugging you can write the code in a file marco.sh and (re)load the definitions to your shell by executing source marco.sh.
+# Write bash functions marco and polo that do the following.
+# Whenever you execute marco the current working directory should be saved in some manner, then when you execute polo, no matter what directory you are in, polo should cd you back to the directory where you executed marco.
+# For ease of debugging you can write the code in a file marco.sh and (re)load the definitions to your shell by executing source marco.sh.
 
 marco() {
 	current_dir=$(pwd)
