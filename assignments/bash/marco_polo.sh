77f5fad3fb0040c67831521bd9490bfd100814b5
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-09-01 19:15:07 -0400

    added assignment

---
+++ assignments/bash/marco_polo.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+: <<COMMENT
+Write bash functions marco and polo that do the following. Whenever you execute marco the current working directory should be saved in some manner, then when you execute polo, no matter what directory you are in, polo should cd you back to the directory where you executed marco. For ease of debugging you can write the code in a file marco.sh and (re)load the definitions to your shell by executing source marco.sh.
+COMMENT
+
+marco() {
+	current_dir=$(pwd)
+}
+
+polo() {
+	cd "$current_dir" || exit
+}
