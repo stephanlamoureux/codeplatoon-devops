f69c76f0992f3c7c05f36db754f0a9294151cf6a
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-09-02 13:11:51 -0400

    new assignments

--- assignments/bash/basics.sh
+++ assignments/bash/basics.sh
@@ -9,3 +9,46 @@
 # 4. Write a shell script that prompts the user for a name of a file or directory and reports if it is a regular file, a directory, or another type of file. Also perform an ls command against the file or directory with the long listing option.
 
 # 5. Modify the previous script to that it accepts the file or directory name as an argument instead of prompting the user to enter it.
+
+# Assignment 1
+hostname=$(hostname)
+echo "This script is running on $hostname."
+
+# Assignment 2
+if [ -f file_path ]; then
+	echo "file_path passwords are enabled."
+	if [ -w file_path ]; then
+		echo "You have permissions to edit file_path."
+	else
+		echo "You do NOT have permissions to edit file_path."
+	fi
+fi
+
+# Assignment 3
+printf "man\nbear\npig\ndog\ncat\nsheep\n"
+
+# Assignment 4
+read -rp "Enter a file or directory name: " name
+
+if [ -f "$name" ]; then
+	echo "Regular file."
+elif [ -d "$name" ]; then
+	echo "Directory."
+elif [ -e "$name" ]; then
+	echo "Other file."
+else
+	echo "Error."
+fi
+
+ls "$name" -lah
+
+# Assignment 5
+if [ -f "$1" ]; then
+	echo "Regular file."
+elif [ -d "$1" ]; then
+	echo "Directory."
+elif [ -e "$1" ]; then
+	echo "Other file."
+else
+	echo "Error."
+fi
