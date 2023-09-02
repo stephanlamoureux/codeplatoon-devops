f392105519b480394ccc88bdb6a2a1bb1db6dbde
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-09-02 10:21:52 -0400

    added assignment

---
+++ assignments/bash/basics.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# 1. Store the output of the command “hostname” in a variable. Display “This script is running on .” where “” is the output of the “hostname” command.
+
+# 2. Write a shell script to check to see if the file “file_path” exists. If it does exist, display “file_path passwords are enabled.” Next, check to see if you can write to the file. If you can, display “You have permissions to edit “file_path.””If you cannot, display “You do NOT have permissions to edit “file_path””.
+
+# 3. Write a shell script that displays “man”,”bear”,”pig”,”dog”,”cat”,and “sheep” on the screen with each appearing on a separate line. Try to do this in as few lines as possible.
+
+# 4. Write a shell script that prompts the user for a name of a file or directory and reports if it is a regular file, a directory, or another type of file. Also perform an ls command against the file or directory with the long listing option.
+
+# 5. Modify the previous script to that it accepts the file or directory name as an argument instead of prompting the user to enter it.
