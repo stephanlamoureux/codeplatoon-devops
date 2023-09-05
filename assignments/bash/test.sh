46c05a67699025d0eeac3ea9cdee6f87c3cb2d20
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-09-05 18:17:36 -0400

    added comment for error handling

--- assignments/bash/test.sh
+++ assignments/bash/test.sh
@@ -8,6 +8,7 @@ n=$((RANDOM % 100))
 
 if [[ n -eq 42 ]]; then
 	echo "Something went wrong"
+	# >&2 sends the output to stderr instead of stdout
 	echo >&2 "The error was using magic numbers"
 	exit 1
 fi
