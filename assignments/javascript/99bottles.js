5ed3d033092db02e5a15e5cdf7807bf71d4d66d2
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-09-06 19:31:20 -0400

    finished assignment

--- assignments/javascript/99bottles.js
+++ assignments/javascript/99bottles.js
@@ -15,7 +15,7 @@ Take one down and pass it around, 1 bottle of beer on the wall.
 1 bottle of beer on the wall, 1 bottle of beer.
 Take one down and pass it around, no more bottles of beer on the wall.
 No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall...`)
+Go to the store and buy some more, 99 bottles of beer on the wall.`)
 
 // a fancier way, but I think the version above is easier to read and more performant.
 
