83af832443061dbf64d4001bbb6d40db8e6a1161
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-09-06 18:19:15 -0400

    formatted

--- assignments/javascript/99bottles.js
+++ assignments/javascript/99bottles.js
@@ -1,13 +1,13 @@
 let bottlesLeft = 99
 
 while (bottlesLeft > 2) {
-    console.log(
-        `${bottlesLeft} bottles of beer on the wall, ${bottlesLeft} bottles of beer.`
-    )
-    bottlesLeft--
-    console.log(
-        `Take one down and pass it around, ${bottlesLeft} bottles of beer on the wall.`
-    )
+	console.log(
+		`${bottlesLeft} bottles of beer on the wall, ${bottlesLeft} bottles of beer.`
+	)
+	bottlesLeft--
+	console.log(
+		`Take one down and pass it around, ${bottlesLeft} bottles of beer on the wall.`
+	)
 }
 
 console.log(`${bottlesLeft} of beer on the wall, ${bottlesLeft} bottles of beer.
@@ -23,15 +23,15 @@ Go to the store and buy some more, 99 bottles of beer on the wall...`)
 // let bottlesLeft
 
 // for (let i = 99; i > 0; i--) {
-//   if (i === 1) {
-//     bottles =  "bottle"
-//     bottlesLeft = "No bottles of beer on the wall!"
-//   } else {
-//     bottles = "bottles"
-//     bottlesLeft = `${i - 1} ${bottles} of beer on the wall!`
-//   }
-//   console.log(`${i} ${bottles} of beer on the wall,`)
-//   console.log(`${i} ${bottles} of beer,`)
-//   console.log("Take one down, pass it around,")
-//   console.log(bottlesLeft);
+// 	if (i === 1) {
+// 		bottles = 'bottle'
+// 		bottlesLeft = 'No bottles of beer on the wall!'
+// 	} else {
+// 		bottles = 'bottles'
+// 		bottlesLeft = `${i - 1} ${bottles} of beer on the wall!`
+// 	}
+// 	console.log(`${i} ${bottles} of beer on the wall,`)
+// 	console.log(`${i} ${bottles} of beer,`)
+// 	console.log('Take one down, pass it around,')
+// 	console.log(bottlesLeft)
 // }
