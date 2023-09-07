4101ed4815675a701ae453dee850b96893d24a56
Stephan Lamoureux <sa.lamoureux@gmail.com>
2023-09-07 15:25:31 -0400

    added factorial assignment in python

---
+++ assignments/python/factorial.py
@@ -0,0 +1,15 @@
+def factorial(num):
+    if num < 0:
+        return None
+    elif num == 0:
+        return 1
+    else:
+        return factorial(num - 1) * num
+
+
+# tests
+print(factorial(8))  # 40320
+print(factorial(18))  # 6402373705728000
+print(factorial(45))  # 119622220865480194561963161495657715064383733760000000000
+print(factorial(0))  # 1
+print(factorial(-20))  # None
